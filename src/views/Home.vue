fc6c2841f197a2cb403f612939d31b98f7a93bc0
abdul23lm <abdul23lm@gmail.com>
2020-11-22 23:17:05 +0700

    Fix style on Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -23,3 +23,15 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+	.home {
+		display: flex;
+		flex-direction: column;
+
+		#main-section {
+			display: flex;
+			margin-top: 50px;
+		}
+	}
+</style>
